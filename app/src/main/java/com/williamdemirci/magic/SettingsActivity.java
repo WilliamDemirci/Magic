96361e3b1680469ceb9a3a3b8d9e661757d67478
William Demirci <wil.8dev@gmail.com>
2018-11-05 23:11:39 +0100

    settings activity update

--- app/src/main/java/com/williamdemirci/magic/SettingsActivity.java
+++ app/src/main/java/com/williamdemirci/magic/SettingsActivity.java
@@ -6,11 +6,24 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
@@ -19,6 +32,11 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class SettingsActivity extends AppCompatActivity {
     private CircleImageView imageSettingProfile;
     private Uri profileImageUri = null;
+    private EditText usernameSetting;
+    private Button settingSaveButton;
+    private ProgressBar settingSaveProgressBarSetting;
+    private FirebaseAuth mAuth;
+    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +44,41 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
 
         imageSettingProfile = (CircleImageView) findViewById(R.id.imageAccountSetting);
+        usernameSetting = (EditText) findViewById(R.id.usernameSetting);
+        settingSaveButton = (Button) findViewById(R.id.settingSaveButton);
+        settingSaveProgressBarSetting = (ProgressBar) findViewById(R.id.settingSaveProgressBarSetting);
+        mAuth = FirebaseAuth.getInstance();
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
+        settingSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameSetting.getText().toString();
+
+                if(!TextUtils.isEmpty(username)) { // username is mandatory
+                    settingSaveProgressBarSetting.setVisibility(View.VISIBLE);
+                    String userId = mAuth.getCurrentUser().getUid();
+
+                    if(profileImageUri != null) { // if there is an image, we save changes
+                        StorageReference imagePath = mStorageRef.child("profileImages").child(userId);
+                        imagePath.putFile(profileImageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                                if(task.isSuccessful()) {
+//                                Task<Uri> downloadUri = task.getResult().getMetadata().getReference().getDownloadUrl();
+                                    Toast.makeText(SettingsActivity.this, "Changes saved", Toast.LENGTH_SHORT).show();
+
+                                }
+                                else {
+                                    Toast.makeText(SettingsActivity.this, "Error : " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                                settingSaveProgressBarSetting.setVisibility(View.INVISIBLE);
+                            }
+                        });
+                    }
+                }
+            }
+        });
 
         imageSettingProfile.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +95,7 @@ public class SettingsActivity extends AppCompatActivity {
                         // start picker to get image for cropping and then use the image in cropping activity
                         CropImage.activity()
                                 .setGuidelines(CropImageView.Guidelines.ON)
+                                .setCropShape(CropImageView.CropShape.OVAL)
                                 .setAspectRatio(1,1)
 //                                .setMaxCropResultSize()
                                 .start(SettingsActivity.this);
