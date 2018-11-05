88df482c1d7ce02c89fa9ea5ed23631403c6f304
William Demirci <wil.8dev@gmail.com>
2018-11-05 03:06:36 +0100

    maj account settings : - request read storage permission - display default profile image in CircleImageView (https://github.com/hdodenhof/CircleImageView)

--- app/src/main/java/com/williamdemirci/magic/AccountSettingActivity.java
+++ app/src/main/java/com/williamdemirci/magic/AccountSettingActivity.java
@@ -1,13 +1,41 @@
 package com.williamdemirci.magic;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class AccountSettingActivity extends AppCompatActivity {
+    private CircleImageView imageSettingProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        imageSettingProfile = (CircleImageView) findViewById(R.id.imageAccountSetting);
+
+        imageSettingProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // check if we are running on at least Android Marshmallow (Android 6)
+                // to request read storage permission
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if(ContextCompat.checkSelfPermission(AccountSettingActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+//                        Toast.makeText(AccountSettingActivity.this, "Permission denied", Toast.LENGTH_SHORT).show();
+                        ActivityCompat.requestPermissions(AccountSettingActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+                    }
+//                    else {
+//                        Toast.makeText(AccountSettingActivity.this, "You already have permission", Toast.LENGTH_SHORT).show();
+//                    }
+                }
+            }
+        });
     }
 }
