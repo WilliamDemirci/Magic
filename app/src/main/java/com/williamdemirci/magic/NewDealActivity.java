8ce7dec420985f4f201024d7bb58031e3b15a47f
William Demirci <wil.8dev@gmail.com>
2018-11-09 21:24:17 +0100

    - add fields of new deal activity

--- app/src/main/java/com/williamdemirci/magic/NewDealActivity.java
+++ app/src/main/java/com/williamdemirci/magic/NewDealActivity.java
@@ -1,14 +1,32 @@
 package com.williamdemirci.magic;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class NewDealActivity extends AppCompatActivity {
     private Toolbar newDealToolbar;
+    private EditText titleNewDeal;
+    private EditText linkNewDeal;
+    private ImageView imageNewDeal;
+    private EditText priceNewDeal;
+    private EditText normalPriceNewDeal;
+    private EditText shippingCostNewDeal;
+    private EditText discountCodeNewDeal;
+    private Spinner spinnerCategoryNewDeal;
+    private EditText startingDateNewDeal;
+    private EditText endingDateNewDeal;
+    private EditText descriptionNewDeal;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +38,13 @@ public class NewDealActivity extends AppCompatActivity {
         // customize toolbar
         setSupportActionBar(newDealToolbar);
         getSupportActionBar().setTitle("Post a new deal");
+
+        // drop down list
+        spinnerCategoryNewDeal = (Spinner) findViewById(R.id.categoryNewDeal);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.categoryArrays, R.layout.spinner_category_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCategoryNewDeal.setAdapter(adapter);
     }
 
     @Override
@@ -32,10 +57,15 @@ public class NewDealActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId() == R.id.validatePost) {
             Toast.makeText(NewDealActivity.this, "ok button", Toast.LENGTH_LONG).show();
-            return false;
+            return true;
         }
         else{
             return false;
         }
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 }
