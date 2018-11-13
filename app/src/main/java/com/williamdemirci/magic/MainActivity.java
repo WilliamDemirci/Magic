8aa81e85fb81f2e748b4581032f09c95a390716b
William Demirci <wil.8dev@gmail.com>
2018-11-13 04:51:50 +0100

    - bug correction (upload of profile image on SettingsActivity)

--- app/src/main/java/com/williamdemirci/magic/MainActivity.java
+++ app/src/main/java/com/williamdemirci/magic/MainActivity.java
@@ -2,20 +2,28 @@ package com.williamdemirci.magic;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private Toolbar dealToolbar;
     private FloatingActionButton addButton;
+    private FirebaseFirestore db;
+    private String currentUserId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+
 
         // Toolbar
         dealToolbar = (Toolbar) findViewById(R.id.dealToolbar);
@@ -44,6 +55,22 @@ public class MainActivity extends AppCompatActivity {
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if(currentUser == null) {
             loginIntent();
+        }
+        else { // ask to fill Username and profile image
+            currentUserId = mAuth.getCurrentUser().getUid();
+            db.collection("Users").document(currentUserId).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if(task.isSuccessful()) {
+                        if(!task.getResult().exists()) {
+                            settingsIntent();
+                        }
+                    }
+                    else {
+                        Toast.makeText(MainActivity.this, "Error : " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
         }
     }
 
--- app/src/main/java/com/williamdemirci/magic/SettingsActivity.java
+++ app/src/main/java/com/williamdemirci/magic/SettingsActivity.java
@@ -3,6 +3,7 @@ package com.williamdemirci.magic;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -14,7 +15,6 @@ import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
@@ -32,10 +32,14 @@ import com.google.firebase.storage.UploadTask;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+import id.zelory.compressor.Compressor;
 
 public class SettingsActivity extends AppCompatActivity {
     private Toolbar toolbarSettings;
@@ -43,94 +47,26 @@ public class SettingsActivity extends AppCompatActivity {
     private boolean imageHasChanged = false;
     private Uri profileImageUri = null;
     private EditText usernameSetting;
-    private Button settingSaveButton;
     private ProgressBar settingSaveProgressBarSetting;
     private String userId;
     private FirebaseAuth mAuth;
     private StorageReference mStorageRef;
     private FirebaseFirestore db;
+    private Bitmap compressedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        imageSettingProfile = (CircleImageView) findViewById(R.id.imageAccountSetting);
-        usernameSetting = (EditText) findViewById(R.id.usernameSetting);
-        settingSaveButton = (Button) findViewById(R.id.settingSaveButton);
-        settingSaveProgressBarSetting = (ProgressBar) findViewById(R.id.settingSaveProgressBarSetting);
-        mAuth = FirebaseAuth.getInstance();
-        mStorageRef = FirebaseStorage.getInstance().getReference();
-        db = FirebaseFirestore.getInstance();
-        userId = mAuth.getCurrentUser().getUid();
-        toolbarSettings = (Toolbar) findViewById(R.id.settingsToolbar);
+        componentsDeclaration();
+        customizeToolbar();
 
-        // customize toolbar
-        setSupportActionBar(toolbarSettings);
-        getSupportActionBar().setTitle("Settings");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getUsernameAndProfileImageFromDatabase();
+        setProfileImage();
+    }
 
-        // get username and image from database
-        db.collection("Users").document(userId).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if(task.isSuccessful()) {
-                    if (task.getResult().exists()) { // if data exist
-                        String username = task.getResult().getString("username");
-                        String image = task.getResult().getString("image");
-
-                        usernameSetting.setText(username); // get username
-                        profileImageUri = Uri.parse(image);
-
-                        // placeholder image
-                        RequestOptions defaultImageRequest = new RequestOptions();
-                        defaultImageRequest.placeholder(R.mipmap.default_profile);
-
-                        // glide (caching images)
-                        Glide.with(SettingsActivity.this)
-                                .setDefaultRequestOptions(defaultImageRequest)
-                                .load(image).into(imageSettingProfile);
-
-                    }
-                }
-                else {
-                    Toast.makeText(SettingsActivity.this, "Error : " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        settingSaveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final String username = usernameSetting.getText().toString();
-
-                if(imageHasChanged) {
-                    if(!TextUtils.isEmpty(username)) { // username is mandatory
-                        settingSaveProgressBarSetting.setVisibility(View.VISIBLE);
-
-                        if(profileImageUri != null) { // if there is an image, we save changes
-                            StorageReference imagePath = mStorageRef.child("profileImages").child(userId + ".jpg");
-                            imagePath.putFile(profileImageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
-                                @Override
-                                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
-                                    if(task.isSuccessful()) {
-                                        storeFirestore(task, username);
-                                    }
-                                    else {
-                                        Toast.makeText(SettingsActivity.this, "Error : " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                                    }
-                                    settingSaveProgressBarSetting.setVisibility(View.INVISIBLE);
-                                }
-                            });
-                        }
-                    }
-                }
-                else { // if image has not changed
-                    storeFirestore(null, username);
-                }
-            }
-        });
-
+    private void setProfileImage() {
         imageSettingProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -154,11 +90,108 @@ public class SettingsActivity extends AppCompatActivity {
         });
     }
 
-    private void storeFirestore(Task<UploadTask.TaskSnapshot> task, String username) {
+    private void saveSettings() {
+        final String username = usernameSetting.getText().toString();
+
+        if(!TextUtils.isEmpty(username) && profileImageUri != null) { // username and image are mandatory
+//                    if(true) {
+            if(imageHasChanged) {
+                File newImageFile = new File(profileImageUri.getPath());
+                try {
+                    compressedImage = new Compressor(SettingsActivity.this)
+                            .setMaxWidth(125)
+                            .setMaxHeight(125)
+                            .setQuality(75)
+                            .compressToBitmap(newImageFile);
+//                                    .setCompressFormat(Bitmap.CompressFormat.WEBP)
+//                                    .setDestinationDirectoryPath(Environment.getExternalStoragePublicDirectory(
+//                                            Environment.DIRECTORY_PICTURES).getAbsolutePath())
+//                                    .compressToFile(actualImage);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                compressedImage.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                byte[] thumbData = baos.toByteArray();
+
+                userId = mAuth.getCurrentUser().getUid(); // to delete
+//                Toast.makeText(SettingsActivity.this, "userId = " + userId, Toast.LENGTH_SHORT).show();
+                StorageReference image_path = mStorageRef.child("profileImages").child(userId + ".jpg");
+                image_path.putBytes(thumbData).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            storeFirestore(task, username);
+                        }
+                        else {
+                            Toast.makeText(SettingsActivity.this, "Error : " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            } // if(imagesHasChanged)
+            else {
+
+                storeFirestore(null, username);
+
+            }
+        } // if(!TextUtils.isEmpty(username) && profileImageUri != null) { // username and image are mandatory
+        Toast.makeText(SettingsActivity.this, "Changes saved", Toast.LENGTH_SHORT).show();
+    }
+
+    private void getUsernameAndProfileImageFromDatabase() {
+        // get username and image from database
+        db.collection("Users").document(userId).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()) {
+                    if (task.getResult().exists()) { // if data exist
+                        String username = task.getResult().getString("username");
+                        String image = task.getResult().getString("image");
+
+                        usernameSetting.setText(username);
+                        profileImageUri = Uri.parse(image);
+
+                        // placeholder image
+                        RequestOptions defaultImageRequest = new RequestOptions();
+                        defaultImageRequest.placeholder(R.mipmap.default_profile);
+
+                        // glide (caching images)
+                        Glide.with(SettingsActivity.this)
+                                .setDefaultRequestOptions(defaultImageRequest)
+                                .load(image).into(imageSettingProfile);
+                    }
+                }
+                else {
+                    Toast.makeText(SettingsActivity.this, "Error : " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void componentsDeclaration() {
+        imageSettingProfile = (CircleImageView) findViewById(R.id.imageAccountSetting);
+        usernameSetting = (EditText) findViewById(R.id.usernameSetting);
+        settingSaveProgressBarSetting = (ProgressBar) findViewById(R.id.settingSaveProgressBarSetting);
+        mAuth = FirebaseAuth.getInstance();
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        db = FirebaseFirestore.getInstance();
+        userId = mAuth.getCurrentUser().getUid();
+        toolbarSettings = (Toolbar) findViewById(R.id.settingsToolbar);
+    }
+
+    private void customizeToolbar() {
+        // customize toolbar
+        setSupportActionBar(toolbarSettings);
+        getSupportActionBar().setTitle("Settings");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void storeFirestore(@NonNull Task<UploadTask.TaskSnapshot> task, String username) {
         Uri downloadUri;
 
         if(task != null) { // if image has changed
-            downloadUri = task.getResult().getUploadSessionUri(); // problème : l'url stockée sur Firestore n'est pas valide
+            downloadUri = task.getResult().getDownloadUrl(); // problème : l'url stockée sur Firestore n'est pas valide
+//            Toast.makeText(SettingsActivity.this, "downloadUri : " + downloadUri, Toast.LENGTH_SHORT).show();
         }
         else { // if image hasn't changed
             downloadUri = profileImageUri;
@@ -173,12 +206,12 @@ public class SettingsActivity extends AppCompatActivity {
                     mainIntent();
                 }
                 else {
-                    Toast.makeText(SettingsActivity.this, "Error : " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(SettingsActivity.this, "Error : " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
                 }
             }
         });
 //                                Task<Uri> downloadUri = task.getResult().getMetadata().getReference().getDownloadUrl();
-        Toast.makeText(SettingsActivity.this, "Changes saved", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(SettingsActivity.this, "Changes saved", Toast.LENGTH_SHORT).show();
     }
 
     private void imagePicker() {
@@ -192,15 +225,18 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
         if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
             if (resultCode == RESULT_OK) {
                 profileImageUri = result.getUri();
                 imageSettingProfile.setImageURI(profileImageUri);
                 imageHasChanged = true;
-            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
-                Exception error = result.getError();
+            }
+            else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Toast.makeText(SettingsActivity.this, "Error : " + result.getError().getMessage(), Toast.LENGTH_SHORT).show();
             }
         }
     }
@@ -213,6 +249,7 @@ public class SettingsActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() { // on back button pressed (not on toolbar)
+        saveSettings();
         mainIntent();
         super.onBackPressed();
     }
@@ -221,6 +258,7 @@ public class SettingsActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home: // back button
+                saveSettings();
                 mainIntent();
                 return true;
         }
